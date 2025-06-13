62a602a8416eb45170d25c5480722333ca81c5f9
RendonnM <srendonm@correo.iue.edu.co>
2025-06-12 22:19:11 -0500

    feat(workExperience)✨: Add work experience section with dynamic job listings

---
+++ src/lib/constants/workExperience.ts
@@ -0,0 +1,30 @@
+export const workExperience = [
+  {
+    company: 'IGGA S.A.S.',
+    position: 'Profesional A (Desarrollador de Software)',
+    duration: 'Jul. 2024 - Actualidad',
+    location: 'Medellín, Colombia',
+    description: `
+Desarrollo de aplicaciones web utilizando tecnologías como HTML, CSS, JavaScript, TypeScript y React, integrando backend con Python Django REST Framework.
+
+Diseño y prototipado de interfaces con Figma, asegurando una experiencia de usuario intuitiva y atractiva.
+
+Creación de dashboards interactivos en Power BI para visualización de datos y apoyo en la toma de decisiones estratégicas.
+
+Implementación de soluciones de automatización de procesos con Power Platform (Power Automate, Power Apps) y UIPath.
+    `
+  },
+  {
+    company: 'IGGA S.A.S.',
+    position: 'Profesional A (Analista de Redes)',
+    duration: 'Nov. 2022 - Jul. 2024',
+    location: 'Medellín, Colombia',
+    description: `Dar soporte a herramientas de georreferenciación (Smallworld), asegurando continuidad operativa y satisfacción de las necesidades del negocio de las telecomunicaciones.
+
+Gestionar incidencias técnicas, reduciendo significativamente los tiempos de respuesta y mejorando la eficiencia operativa del área.
+
+Implementar mejoras continuas en los procesos internos relacionados con la gestión geoespacial, facilitando la obtención de información estratégica confiable para la toma de decisiones empresariales.
+
+Realizar capacitaciones al equipo sobre el uso avanzado de Smallworld, mejorando la calidad y precisión en la gestión de datos.`
+  }
+]
