777abb4ce1337fc4b7f36a617ad8aadb1e9b535a
RendonnM <srendonm@correo.iue.edu.co>
2025-07-08 12:29:39 -0500

    feat(mouse-follower) ✨: Add MouseFollower component and small improvements

---
+++ src/components/MouseFollower.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useCallback, useState } from 'react'
+
+export const MouseFollower = () => {
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 })
+
+  const handleMouseMove = useCallback((e: MouseEvent) => {
+    setCoordinates({ x: e.pageX, y: e.pageY })
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove)
+    return () => window.removeEventListener('mousemove', handleMouseMove)
+  }, [handleMouseMove])
+
+  return (
+    <div
+      className="absolute h-32 w-32 bg-white/22 rounded-full blur-3xl pointer-events-none transition-all duration-75 ease-out "
+      style={{
+        left: `${coordinates.x}px`,
+        top: `${coordinates.y}px`,
+        transform: 'translate(-50%, -50%)'
+      }}
+    />
+  )
+}
