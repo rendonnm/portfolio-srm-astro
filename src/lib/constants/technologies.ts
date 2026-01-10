bae95469498bb64346cf0be9543accd5f08b1d26
RendonnM <srendonm@correo.iue.edu.co>
2026-01-10 14:10:02 -0500

    feat(technologies)✨: add technologies carousel for bento section

---
+++ src/lib/constants/technologies.ts
@@ -0,0 +1,69 @@
+import JavaScript from "@/assets/javascript.svg";
+import TypeScript from "@/assets/typescript.svg";
+import React from "@/assets/react.svg";
+import Tailwind from "@/assets/tailwind.svg";
+import NestJS from "@/assets/nestjs.svg";
+import Astro from "@/assets/astro.svg";
+import Vite from "@/assets/vite.svg";
+import Vitest from "@/assets/vitest.svg";
+import Playwright from "@/assets/playwright.svg";
+import Jest from "@/assets/jest.svg";
+import TanStack from "@/assets/tanstack.svg";
+
+export const technologies = [
+  {
+    name: "JavaScript",
+    logo: JavaScript,
+    href: "",
+  },
+  {
+    name: "TypeScript",
+    logo: TypeScript,
+    href: "",
+  },
+  {
+    name: "React",
+    logo: React,
+    href: "",
+  },
+  {
+    name: "Astro",
+    logo: Astro,
+    href: "",
+  },
+  {
+    name: "Nest JS",
+    logo: NestJS,
+    href: "",
+  },
+  {
+    name: "Tailwind CSS",
+    logo: Tailwind,
+    href: "",
+  },
+  {
+    name: "Vite",
+    logo: Vite,
+    href: "",
+  },
+  {
+    name: "Vitest",
+    logo: Vitest,
+    href: "",
+  },
+  {
+    name: "TanStack",
+    logo: TanStack,
+    href: "",
+  },
+  {
+    name: "Playwright",
+    logo: Playwright,
+    href: "",
+  },
+  {
+    name: "Jest",
+    logo: Jest,
+    href: "",
+  },
+];
