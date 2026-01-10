45ff8adedd535ebb926d589176de7d39a14be8c0
RendonnM <srendonm@correo.iue.edu.co>
2026-01-10 15:52:21 -0500

    fix(config)🔧: update import statements and formatting for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, 'src')
-      }
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   integrations: [react(), mdx()],
-  adapter: vercel()
+  adapter: vercel(),
+  output: "server",
 });
