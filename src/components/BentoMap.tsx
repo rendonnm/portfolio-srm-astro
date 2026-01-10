99706dae37aecfb78ca99ac1eba4293d6a955c2f
RendonnM <srendonm@correo.iue.edu.co>
2026-01-10 15:08:39 -0500

    feat(bento)✨: improve layout structure and styling for better readability

--- src/components/BentoMap.tsx
+++ src/components/BentoMap.tsx
@@ -2,7 +2,7 @@ import { Map as MapCn, MapControls } from "@/components/ui/map";
 
 export function BentoMap() {
   return (
-    <div className="dark w-full h-full">
+    <div className="dark w-full h-full min-h-50">
       <MapCn center={[-75.587374, 6.170049]} zoom={13.9}>
         <MapControls position="bottom-right" showZoom />
       </MapCn>
