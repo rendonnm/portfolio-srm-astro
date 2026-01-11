043c3c23ed9a26d84985d23fcffe25cbf2567b2f
RendonnM <srendonm@correo.iue.edu.co>
2026-01-11 17:06:22 -0500

    fix(cronometer)✨: initialize time state correctly and enhance formatting for better readability

--- src/components/Cronometer.tsx
+++ src/components/Cronometer.tsx
@@ -14,16 +14,24 @@ function getFormatedTime() {
 }
 
 export function Cronometer() {
-  const [time, setTime] = useState(() => getFormatedTime());
+  const [time, setTime] = useState("00:00:00");
 
   useEffect(() => {
-    setInterval(() => {
-      setTime(() => getFormatedTime());
+    setTime(getFormatedTime());
+
+    const id = setInterval(() => {
+      setTime(getFormatedTime());
     }, 1000);
+
+    return () => clearInterval(id);
   }, []);
 
   return (
-    <p className="sm:text-5xl text-6xl font-semibold text-zinc-300 clock-font">
+    <p
+      className="sm:text-5xl text-6xl font-semibold text-gradient clock-font
+      bg-linear-to-r from-cyan-100 via-blue-200 to-indigo-200 bg-clip-text
+		  text-transparent"
+    >
       {time}
     </p>
   );
