6575ec61dd0d7e3c6848db77f388ad25d814ad21
RendonnM <srendonm@correo.iue.edu.co>
2026-09-13 11:10:34 -0500

    feat(BentoCard): add shadow effect to BentoCard component

--- src/components/GitHubDaySquare.tsx
+++ src/components/GitHubDaySquare.tsx
@@ -8,5 +8,9 @@ interface GitHubDayProps {
 export function GitHubDaySquare({ level }: GitHubDayProps) {
   const bgColor = gitHubColors[level];
 
-  return <div className={`size-full ${bgColor} rounded-xs`}></div>;
+  return (
+    <div
+      className={`size-full ${bgColor} rounded-xs pointer-events-none`}
+    ></div>
+  );
 }
