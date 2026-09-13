df41ef33557c4c5ffc3ce42e92388d64ce14a5d3
RendonnM <srendonm@correo.iue.edu.co>
2026-09-13 11:35:57 -0500

    feat(config): update site URL to new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  site: "https://santiagorendon.vercel.app",
+  site: "https://santiagorendon.dev",
   vite: {
     resolve: {
       alias: {
